b9d1a2edb6ec12dd19fd10cb18f7ad8653ffea60
Andynap <vybiral7@gmail.com>
2015-07-22 19:06:54 +0200

    Project upload

---
+++ Graf_World/Graf_World/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Graf_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericStartX = new System.Windows.Forms.NumericUpDown();
+            this.numericStartY = new System.Windows.Forms.NumericUpDown();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStartX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStartY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 249);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericStartX
+            // 
+            this.numericStartX.Location = new System.Drawing.Point(52, 267);
+            this.numericStartX.Name = "numericStartX";
+            this.numericStartX.Size = new System.Drawing.Size(49, 20);
+            this.numericStartX.TabIndex = 2;
+            this.numericStartX.ValueChanged += new System.EventHandler(this.numericStartX_ValueChanged);
+            // 
+            // numericStartY
+            // 
+            this.numericStartY.Location = new System.Drawing.Point(52, 293);
+            this.numericStartY.Name = "numericStartY";
+            this.numericStartY.Size = new System.Drawing.Size(49, 20);
+            this.numericStartY.TabIndex = 3;
+            this.numericStartY.ValueChanged += new System.EventHandler(this.numericStartY_ValueChanged);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelX.Location = new System.Drawing.Point(25, 267);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(21, 17);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "X:";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelY.Location = new System.Drawing.Point(25, 292);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(21, 17);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 332);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.numericStartY);
+            this.Controls.Add(this.numericStartX);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStartX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStartY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericStartX;
+        private System.Windows.Forms.NumericUpDown numericStartY;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+    }
+}
+
---
+++ Graf_World/Graf_World/FormGraf.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graf_World
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap graphBitMap;
+        private List<Rectangle> listRec = new List<Rectangle>();//list čtverců s názvem listRec
+        private Graphics g;
+        private Font Fontik = new Font(new FontFamily("Arial"), 15F, FontStyle.Regular);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        //pokud není navrátová hodnota píšu void, pokud je nevratová hodnota místo voidu píšu konkrétní datový typ např. string, int
+        private void drawListRec(List<Rectangle> _listRec)
+        {
+            foreach (Rectangle index in _listRec) //index určuje která položka z listu se vykreslí
+            {
+                g.FillRectangle(Brushes.BlueViolet, index);
+                g.DrawRectangle(Pens.Black, index);
+            }
+            if (_listRec.Count >= 2)
+            {
+                string answer = _listRec.ElementAt(0).IntersectsWith(_listRec.ElementAt(1)).ToString();
+                g.DrawString(answer, Fontik, Brushes.Black, this.graphBitMap.Width - (g.MeasureString(answer, this.Fontik).Width) - 10, 0);
+            }
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.graphBitMap = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+
+  
+            g = Graphics.FromImage(this.graphBitMap); //zde si načtu bit mapu do g
+            Rectangle rec0 = new Rectangle((int)numericStartX.Value, (int)numericStartY.Value, 50, 50); //(int) zajistí aby se nam datový, ořezal na int, pokud to lze
+            listRec.Add(rec0);
+            //ručně přidání do listu
+
+            Rectangle rec2 = new Rectangle(20, 30, 35, 35);
+            listRec.Add(rec2);
+
+            Rectangle rec1 = new Rectangle(20, 30, 20, 20);
+            listRec.Add(rec1);
+                       
+            this.numericStartX.Maximum = this.graphBitMap.Width-rec0.Width;
+            this.numericStartY.Maximum = this.graphBitMap.Height - rec0.Height;
+
+            drawListRec(listRec); //vykreslovací funkce
+                                    
+            this.pictureBox1.BackgroundImage = graphBitMap;
+        }
+
+        private void numericStartX_ValueChanged(object sender, EventArgs e)
+        {
+            g.Clear(this.pictureBox1.BackColor);
+
+            Rectangle docasna = listRec.ElementAt(0);
+            docasna.X = (int)numericStartX.Value;
+            listRec[0] = docasna;
+            drawListRec(listRec);
+
+            pictureBox1.Refresh();
+        }
+
+        private void numericStartY_ValueChanged(object sender, EventArgs e)
+        {
+            g.Clear(this.pictureBox1.BackColor);
+            Rectangle docasna = listRec.ElementAt(0);
+            docasna.Y = (int)numericStartY.Value;
+            listRec[0] = docasna;
+            drawListRec(listRec);
+
+            pictureBox1.Refresh();
+        }
+    }
+}
